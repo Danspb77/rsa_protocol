83cc400a0fa5cf1c1cb822ee153641207a1146a5
Danya <dan.shell@bk.ru>
2023-06-13 20:01:58 +0300

    rsa  working in 70 % cases

--- rsa.py
+++ rsa.py
@@ -26,27 +26,30 @@ def eiler_function(n,q):
 
 #  generate e
 def e_generate(fi):
+    fi=100
     e=random.randint(1,fi)
     while not coprime2(e,fi):
         e=random.randint(1,fi)
     return e
 
-# def d_generate(e,fi):
-#     d=random.randint(2,e)
-#     while not ((d*e)%fi==1):
-#         d=random.randint(2,q)
-#     return d 
+def d_generate(e,fi):
+    d=random.randint(2,1000)
+    while not ((d*e)%fi==1):
+        d=random.randint(2,q)
+    return d 
 
 
 p=(simp_rand_num())
 q=(simp_rand_num())
+n=p*q
 fi=eiler_function(p,q)
 e=e_generate(fi)
 print(e)
-# d=d_generate(open_exp,fi)
-# print(d)
+d=d_generate(e,fi)
+print(d)
 print(fi) 
-
+print("open key" ,e, n)
+print("close" ,d,n )
 
 # print((open_exp*d)%fi)
 
